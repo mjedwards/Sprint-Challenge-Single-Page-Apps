43640d6a07c527fd52804d966bcc81c6eec9f2d9
mjedwards <jairedwards20@gmail.com>
2019-11-08 21:56:08 -0500

    complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
-import Header from "./components/Header.js";
+import { Route } from "react-router-dom";
 
+import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage";
+import FormikSearchForm from "./components/SearchForm";
+import CharacterList from "./components/CharacterList";
+import CharacterCard from "./components/CharacterCard.js";
 
 export default function App() {
   return (
     <main>
       <Header />
+      <WelcomePage />
+      <FormikSearchForm />
+      <Route exact path='/' render={props => <CharacterList {...props} />} />
+      <Route
+        path='/character/:id'
+        render={props => <CharacterCard {...props} />}
+      />
     </main>
   );
 }
